7f402941f157678a1cd508b52d789041d608abe2
BrnCalisario <113780552+Barbs2103@users.noreply.github.com>
2022-11-01 17:10:30 -0300

    zombie sprites

--- enemy.py
+++ enemy.py
@@ -1,10 +1,28 @@
-from pygame import sprite, Surface
+import pygame
 
-class Enemy(sprite.Sprite):
+
+
+
+
+
+
+class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos=(0, 0)):
         super().__init__()
-        self.image = Surface((50, 50))
-        self.image.fill("green")
+        
+        zombie_walk1 = pygame.image.load("graphics/zombie/zombie_walk1.png").convert_alpha()
+        zombie_walk2 = pygame.image.load("graphics/zombie/zombie_walk2.png").convert_alpha()
+        zombie_walk3 = pygame.image.load("graphics/zombie/zombie_walk3.png").convert_alpha()
+
+
+
+        self.zombie_walk = [zombie_walk1, zombie_walk2, zombie_walk3]
+        self.zombie_index = 0
+
+        self.image = self.zombie_walk[self.zombie_index]
+
+        # self.image = Surface((50, 50))
+        # self.image.fill("green")
         self.rect = self.image.get_rect(topleft=pos)
         
         self.damage_delay = 0
@@ -15,6 +33,7 @@ class Enemy(sprite.Sprite):
 
     def update(self):
         self.rect.x -= self.speed
+        self.animation_state()
         self.destroy()
 
     def destroy(self):
@@ -32,4 +51,8 @@ class Enemy(sprite.Sprite):
             self.damage_delay = 0
             plant.recive_damage(self.damage)
         else: self.damage_delay += 5
-    
+
+    def animation_state(self):
+        self.zombie_index += 0.05
+        if self.zombie_index >= len(self.zombie_walk) : self.zombie_index = 0
+        self.image = self.zombie_walk[int(self.zombie_index)]
